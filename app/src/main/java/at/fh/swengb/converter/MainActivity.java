b306662c937951768df8a9c121350dc3033b1a7a
Markus Wolf <markus.wolf@koerbler.com>
2016-11-16 09:15:30 +0100

    Initial commit

---
+++ app/src/main/java/at/fh/swengb/converter/MainActivity.java
@@ -0,0 +1,46 @@
+package at.fh.swengb.converter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String[] spinnerItems;
+    private Spinner spinner;
+    private EditText etInput;
+    private TextView tvResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etInput = (EditText) findViewById(R.id.etInput);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        spinnerItems = new String[] {"inch","cm"};
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, spinnerItems);
+        spinner.setAdapter(adapter);
+    }
+
+    public void calculate(View view){
+        Double value = Double.parseDouble(etInput.getText().toString());
+        String valueString = "";
+        if(spinner.getSelectedItem().toString()=="inch"){
+            value = value * 2.54;
+            valueString = String.format("%.6f",value);
+            tvResult.setText("Result: "+valueString+" cm");
+        }
+        if(spinner.getSelectedItem().toString()=="cm"){
+            value = value / 2.54;
+            valueString = String.format("%.6f",value);
+            tvResult.setText("Result: "+valueString+" inch");
+        }
+
+    }
+}
